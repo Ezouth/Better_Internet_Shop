4d2f977ff54fca35a2adc1a3c0fdf19eaa1b0c32
Tyler Whidden <Ty.Whidden@gmail.com>
2018-10-01 11:28:35 -0400

    Finished row functionality to show three products per row, TODO: add margins between cards, removed filler from index.html

--- static/js/app.js
+++ static/js/app.js
@@ -11,6 +11,10 @@ function showProducts(response) {
 
   // need to loop through the products and create card for each
   for (let i=0; i < products.length; i++) {
+    //open row for each third product
+    if ( i % 3 == 0) {
+      html += '<div class="row">';
+    }
     html +=` <div class="card col-md-4">
       <div class="card-img-top">
           <img src="http://placehold.it/1x1" alt="Placeholder" class="card-img" />
@@ -20,6 +24,10 @@ function showProducts(response) {
         <div class="card-text">${products[i].description}</div>
         <button onclick="addToCart(${products[i].id})" class="btn btn-primary">Add To Cart</button>
       </div>` ;
+    // close row after 3 products have been added
+    if ((i + 1) % 3 == 0 ) {
+      html += '</div>';
+    }
   }
   // inject html string into products id
   $("#products").html(html);
