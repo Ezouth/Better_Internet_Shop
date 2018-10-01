e3b256d88bce51efddab76eb9b43b194906f0a34
Tyler Whidden <Ty.Whidden@gmail.com>
2018-10-01 11:19:25 -0400

    Showing products properly, TODO: add row functionality for 3 products per row

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,37 @@
 $.get('./components/header.html', function(response) {
     $("#nav").html(response);
 });
+
+// Callback function to show our products.json
+function showProducts(response) {
+  console.log(response);
+  // define local variables for headers and html
+  let products = response.products;
+  let html = "";
+
+  // need to loop through the products and create card for each
+  for (let i=0; i < products.length; i++) {
+    html +=` <div class="card col-md-4">
+      <div class="card-img-top">
+          <img src="http://placehold.it/1x1" alt="Placeholder" class="card-img" />
+        </div>
+        <div class="card-title">${products[i].title}</div>
+        <div class="card-subtitle">$${products[i].price}</div>
+        <div class="card-text">${products[i].description}</div>
+        <button onclick="addToCart(${products[i].id})" class="btn btn-primary">Add To Cart</button>
+      </div>` ;
+  }
+  // inject html string into products id
+  $("#products").html(html);
+}
+
+
+// use jQuery to pull products information
+
+$.get('./assests/products.json', showProducts);
+
+
+// Add funcitonality later
+function addToCart(id) {
+  console.log(id);
+}
